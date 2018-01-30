fb9af789ca0a47bf11740b1bc714c255227d7171
mvvucak <wepster34@hotmail.com>
2018-01-30 00:18:35 +0000

    Chapter 5 Exercises

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -24,16 +24,16 @@ def populate():
 
     other_pages = [
         {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-        {"title": "Flask", "url":"http://flask.pocoo.org"}
+          {"title": "Flask", "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages},
-        "Django": {"pages": django_pages},
-        "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32}}
 
 
-    for cat, cat_data in cats.items():
-        c = add_cat(cat)
+    for cat,cat_data in cats.items():
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -48,8 +48,10 @@ def  add_page(cat, title, url, views = 0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
